4555e9e3699f6e16d38fab796f0beede5fbf5473
Prakash4844 <pk484442@gmail.com>
2023-04-01 15:40:52 +0530

    All code updated except chapter 10 and 11

--- Project 2/Main_MyAttempt.py
+++ Project 2/Main_MyAttempt.py
@@ -18,15 +18,15 @@ while (userguess != randomNumber):
         print("Your Guess is Too Low, Pick a Higher Number.")
 
     elif (userguess == randomNumber):
-        print("Woah! You guessed it correctly!")
+        print("Wow! You guessed it correctly!")
 
 print(f"You took {guesscount} attempts to guess the no. correctly.")
 
-with open("Project 2//High_Score", "r") as f:
+with open("Project 2/High_Score", "r") as f:
     High_Score = int(f.read())
 
 if (High_Score > guesscount):
     print("Wow! Congratulations, You have made a new HiGH SCORE.")
 
-    with open("Project 2//High_Score", "w") as f:
+    with open("Project 2/High_Score", "w") as f:
         f.write(str(guesscount))
